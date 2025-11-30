d843046ecc47c849c8d78a12b1cb0f2051125686
James <jdchester7@sky.com>
2025-11-30 17:13:41 +0000

    Read and output file with execution timer

--- AOC2025-Prep/wordcount/Program.cs
+++ AOC2025-Prep/wordcount/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Diagnostics;
+
+var watch = System.Diagnostics.Stopwatch.StartNew();
+
+try
+{
+    using StreamReader reader = new("200wordsample.txt");
+
+    string text = reader.ReadToEnd();
+
+    Console.WriteLine(text);
+}
+catch (IOException e)
+{
+    Console.WriteLine("The file could not be read:");
+    Console.WriteLine(e.Message);
+}
+
+watch.Stop();
+
+var elapsedMs = watch.ElapsedMilliseconds;
+
+Console.WriteLine($"Time Taken {elapsedMs} ms");
