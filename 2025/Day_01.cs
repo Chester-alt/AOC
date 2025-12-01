7006d9e346bcf437beb2b0ded0f0938bd7130f06
James <jdchester7@sky.com>
2025-12-01 13:50:36 +0000

    Scaffold configured for 2025

---
+++ 2025/Day_01.cs
@@ -0,0 +1,30 @@
+namespace aoc;
+
+public static class Day_01
+{
+    public static int Part1(SolutionTimer timer, string[] input)
+    {
+        timer.StartParsing();
+
+        var zeroCount = 0;
+
+
+        timer.Stop();
+
+        return zeroCount;
+
+    }
+}
+    
+
+//    public static int Part2(SolutionTimer timer, string[] input)
+//    {
+//        timer.StartParsing();
+//
+//       
+//
+//        timer.Stop();
+//
+//       
+//    }
+//}
