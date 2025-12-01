3048ccdbb3db90cfab5c1a1194a9ed6025f48709
James <jdchester7@sky.com>
2025-12-01 23:16:05 +0000

    Streamed the input directly and removed empty entries cause by delimiters

--- AOC2025-Prep/wordcount/Program.cs
+++ AOC2025-Prep/wordcount/Program.cs
@@ -5,27 +5,32 @@ var watch = System.Diagnostics.Stopwatch.StartNew();
 
 try
 {
-    using StreamReader reader = new("200wordsample.txt");
+    using var reader = new StreamReader("200wordsample.txt");
 
-    Dictionary<string, int> stats = new Dictionary<string, int>();
-    string text = reader.ReadToEnd();
-    char[] chars = { ' ', ',', '.', ':', '?', ';', '!', '\n', '\r', '\t' };
-    // Split the words 
-    string[] words = text.Split(chars);
+    var stats = new Dictionary<string, int>();
+    char[] seperators = { ' ', ',', '.', ':', '?', ';', '!', '\n', '\r', '\t' };
+    
     int minWordLength = 2;
 
-    foreach (string word in words)
+    string? line;
+    while ((line = reader.ReadLine()) != null)
     {
-        string w = word.Trim().ToLower();
-        if (w.Length > minWordLength)
+       string[] words = line.Split(seperators, StringSplitOptions.RemoveEmptyEntries);
+
+        foreach (string word in words)
         {
-            // Avoiding double lookups 
-            if (stats.TryGetValue(w, out int count))
-                stats[w] = count + 1;
-            else
-                stats[w] = 1;
+            string w = word.Trim().ToLower();
+            if (w.Length > minWordLength)
+            {
+                // Avoiding double lookups 
+                if (stats.TryGetValue(w, out int count))
+                    stats[w] = count + 1;
+                else
+                    stats[w] = 1;
+            }
         }
     }
+    
     // Order the list by word occurrence 
     var orderedStats = stats.OrderByDescending(x => x.Value);
     // Print total word count 
