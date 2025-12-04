eca729c5b379e8a76d6d90cd3b32a5b7e2135726
James <jdchester7@sky.com>
2025-12-04 13:54:27 +0000

    Day 3 part 2

--- 2025/Day_03.cs
+++ 2025/Day_03.cs
@@ -1,5 +1,7 @@
+using System.ComponentModel;
 using System.Data.Common;
 using System.Diagnostics.CodeAnalysis;
+using System.Net.Mail;
 using System.Reflection.Metadata.Ecma335;
 
 namespace aoc;
@@ -9,6 +11,9 @@ public static class Day_03
     public static long Part1(SolutionTimer timer, string[] input)
     {
         long total = 0;
+
+        timer.StartParsing();
+        timer.StartExecuting();
 
         foreach (var line in input)
         {
@@ -29,6 +34,7 @@ public static class Day_03
             total += best;
         }
 
+        timer.Stop();
         return total;
     }
 
@@ -38,7 +44,39 @@ public static class Day_03
 
     public static long Part2(SolutionTimer timer, string[] input)
     {
-        return 0;
+        long total = 0;
+
+        timer.StartParsing();
+        timer.StartExecuting();
+
+        foreach (var line in input)
+        {
+            int k = 12; // Max digit
+            int toDrop = line.Length - k;
+            var stack = new List<char>();
+
+            foreach (char c in line)
+            {
+                while (stack.Count > 0 && toDrop > 0 && stack[^1] < c)
+                {
+                    stack.RemoveAt(stack.Count - 1);
+                    toDrop--;
+                }
+                stack.Add(c);
+            }
+
+            while (toDrop > 0)
+            {
+                stack.RemoveAt(stack.Count - 1);
+                toDrop--;
+            }
+
+            string best = new string(stack.Take(k).ToArray());
+            total += long.Parse(best);
+        }
+
+        timer.Stop();   
+        return total;      
     }
 
 }
