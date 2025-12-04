3f790566f69e9dd2dfd2059a5ff0a18c17cd045e
James <jdchester7@sky.com>
2025-12-04 15:20:39 +0000

    Day 4 part 1 complete

--- 2025/Day_04.cs
+++ 2025/Day_04.cs
@@ -1,4 +1,5 @@
 using System.Data.Common;
+using System.Runtime.CompilerServices;
 
 namespace aoc;
 
@@ -6,7 +7,46 @@ public static class Day_04
 {
     public static long Part1(SolutionTimer timer, string[] input)
     {
-        return 0;
+        int accessibleCount = 0;
+
+        timer.StartParsing();
+        timer.StartExecuting();
+
+        int[][] directions = new int[][]
+        {
+            new[]{-1, -1}, new[]{-1, 0}, new[]{-1, 1},
+            new[]{0, -1},                 new[]{0, 1},
+            new[]{1, -1},  new[]{1, 0},  new[]{1, 1}
+        };
+
+
+        for (int r = 0; r < input.Length; r++)
+        {
+            for (int c = 0; c < input[r].Length; c++)
+            {
+                if (input[r][c] != '@') continue;
+
+                int neighborRolls = 0;
+                foreach (var dir in directions)
+                {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if (nr >= 0 && nr < input.Length &&
+                        nc >= 0 && nc < input[nr].Length &&
+                        input[nr][nc] == '@')
+                    {
+                        neighborRolls++;
+                    }
+                }
+
+                if (neighborRolls < 4)
+                    accessibleCount++;
+            }
+        }
+
+        timer.Stop();   
+        return accessibleCount;
     }
 
 
