f4da67a30b8b82f442dbb6f54ae37e023fc77b2e
James <jdchester7@sky.com>
2025-12-04 13:16:47 +0000

    Day 3 part 1

--- 2025/Day_03.cs
+++ 2025/Day_03.cs
@@ -1,4 +1,6 @@
 using System.Data.Common;
+using System.Diagnostics.CodeAnalysis;
+using System.Reflection.Metadata.Ecma335;
 
 namespace aoc;
 
@@ -6,8 +8,30 @@ public static class Day_03
 {
     public static long Part1(SolutionTimer timer, string[] input)
     {
-        return 0;
+        long total = 0;
+
+        foreach (var line in input)
+        {
+            var digits = line.Select(c => c - '0').ToArray();
+            int best = 0;
+
+            for (int i = 0; i < digits.Length; i++)
+            {
+                int first = digits[i];
+                int second = digits.Skip(i + 1).DefaultIfEmpty(-1).Max();
+
+                if (second == -1) continue;
+
+                int candidate = first * 10 + second;
+                if (candidate > best) best = candidate;
+            }
+
+            total += best;
+        }
+
+        return total;
     }
+
 
 
 
