91e4591da4bd55799aadd79f48489d99c80b1de0
James <jdchester7@sky.com>
2025-12-05 16:14:20 +0000

    Day 5 part 1 complete

--- 2025/Day_05.cs
+++ 2025/Day_05.cs
@@ -6,7 +6,40 @@ public class Day_05
 {
     public static long Part1(SolutionTimer timer, string [] input)
     {
-        return 0;
+        timer.StartParsing();
+        timer.StartExecuting();
+        var ranges = new List<(long start, long end)>();
+        int i = 0;
+        
+        // Parse the ranges until blank line  
+        for (; i < input.Length; i++)
+        {
+            if (string.IsNullOrWhiteSpace(input[i]))
+                break;
+            
+            var parts = input[i].Split('-');
+            long start  = long.Parse(parts[0]);
+            long end = long.Parse(parts[1]);
+            ranges.Add((start, end));
+        }
+        // Parse the IDs
+        var ids = new List<long>();
+        for (i = i +  1; i < input.Length; i++)
+        {
+            if (!string.IsNullOrWhiteSpace(input[i]))
+                ids.Add(long.Parse(input[i]));
+        }
+
+        int freshCount = 0;
+
+        foreach (var id in ids)
+        {
+            bool isFresh = ranges.Any(r => id >= r.start && id <= r.end);
+            if(isFresh)
+                freshCount++;
+        }
+        timer.Stop();
+        return freshCount;
     }
 
     public static long Part2(SolutionTimer timer, string[] input)
