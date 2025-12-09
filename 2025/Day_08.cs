0e06711f41b5ae4429e854614eb91c55b731ccf4
James <jdchester7@sky.com>
2025-12-09 22:56:47 +0000

    Day 08 part 2 not tested - compute time too long

--- 2025/Day_08.cs
+++ 2025/Day_08.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics;
+using System.Formats.Tar;
 using System.Runtime.CompilerServices;
 using aoc;
 
@@ -84,11 +86,71 @@ public class Day_08
 
     public static long Part2(SolutionTimer timer, string[] input)
     {
-        timer.StartParsing();
-        timer.StartExecuting();
         
-        timer.Stop();
-        return 0;
+        // Parse the input
+        var points = input
+            .Where(line => !string.IsNullOrWhiteSpace(line))
+            .Select(line =>
+            {
+                var parts = line.Split(',');
+                return (X: int.Parse(parts[0]),
+                    Y: int.Parse(parts[1]),
+                    Z: int.Parse(parts[2]));
+            })
+            .ToList(); 
+     
+        // Union-find
+        var parent = Enumerable.Range(0, points.Count).ToArray();
+        var size   = Enumerable.Repeat(1, points.Count).ToArray();
+
+        int Find(int x)
+        {
+            if (parent[x] != x) parent[x] = Find(parent[x]);
+            return parent[x];
+        }
+
+        bool Union(int a, int b)
+        {
+            a = Find(a);
+            b = Find(b);
+            if (a == b) return false;
+            if (size[a] < size[b]) (a, b) = (b, a);
+            parent[b] = a;
+            size[a] += size[b];
+            return true;
+        }
+        
+        // Build edges directly into a priority queue 
+        var pq = new PriorityQueue<(int A, int B, long Dist2), long>();
+
+        for (int i = 0; i < points.Count; i++)
+        {
+            for (int j = i + 1; j < points.Count; j++)
+            {
+                long dx = points[j].X - points[i].X;
+                long dy = points[j].Y - points[i].Y;
+                long dz = points[j].Z - points[i].Z;
+                long d2 = dx * dx + dy * dy + dz * dz;
+                pq.Enqueue((i, j, d2), d2);
+            }
+        }
+        // Continue until fully connected 
+        int componentsRemaining = points.Count;
+        (int A, int B) lastEdge = default;
+
+        while (componentsRemaining > 1 && pq.Count > 0)
+        {
+            var edge = pq.Dequeue();
+            if (Union(edge.A, edge.B))
+            {
+                componentsRemaining--;
+                lastEdge = (edge.A, edge.B);
+            }
+        }
+
+        long answer = (long)points[lastEdge.A].X * (long)points[lastEdge.B].X;
+        
+        return answer;
     }
     
 }
